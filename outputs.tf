aa4fd080945ceb36f4a9a3c361ed65b3eac532df
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:37 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,55 +1,50 @@
-output "workloads_sap_three_tier_virtual_instances" {
-  description = "All workloads_sap_three_tier_virtual_instance resources"
-  value       = azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances
-  sensitive   = true
-}
 output "workloads_sap_three_tier_virtual_instances_app_location" {
-  description = "List of app_location values across all workloads_sap_three_tier_virtual_instances"
-  value       = [for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : v.app_location]
+  description = "Map of app_location values across all workloads_sap_three_tier_virtual_instances, keyed the same as var.workloads_sap_three_tier_virtual_instances"
+  value       = { for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : k => v.app_location }
 }
 output "workloads_sap_three_tier_virtual_instances_environment" {
-  description = "List of environment values across all workloads_sap_three_tier_virtual_instances"
-  value       = [for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : v.environment]
+  description = "Map of environment values across all workloads_sap_three_tier_virtual_instances, keyed the same as var.workloads_sap_three_tier_virtual_instances"
+  value       = { for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : k => v.environment }
 }
 output "workloads_sap_three_tier_virtual_instances_identity" {
-  description = "List of identity values across all workloads_sap_three_tier_virtual_instances"
-  value       = [for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : v.identity]
+  description = "Map of identity values across all workloads_sap_three_tier_virtual_instances, keyed the same as var.workloads_sap_three_tier_virtual_instances"
+  value       = { for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : k => v.identity }
 }
 output "workloads_sap_three_tier_virtual_instances_location" {
-  description = "List of location values across all workloads_sap_three_tier_virtual_instances"
-  value       = [for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : v.location]
+  description = "Map of location values across all workloads_sap_three_tier_virtual_instances, keyed the same as var.workloads_sap_three_tier_virtual_instances"
+  value       = { for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : k => v.location }
 }
 output "workloads_sap_three_tier_virtual_instances_managed_resource_group_name" {
-  description = "List of managed_resource_group_name values across all workloads_sap_three_tier_virtual_instances"
-  value       = [for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : v.managed_resource_group_name]
+  description = "Map of managed_resource_group_name values across all workloads_sap_three_tier_virtual_instances, keyed the same as var.workloads_sap_three_tier_virtual_instances"
+  value       = { for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : k => v.managed_resource_group_name }
 }
 output "workloads_sap_three_tier_virtual_instances_managed_resources_network_access_type" {
-  description = "List of managed_resources_network_access_type values across all workloads_sap_three_tier_virtual_instances"
-  value       = [for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : v.managed_resources_network_access_type]
+  description = "Map of managed_resources_network_access_type values across all workloads_sap_three_tier_virtual_instances, keyed the same as var.workloads_sap_three_tier_virtual_instances"
+  value       = { for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : k => v.managed_resources_network_access_type }
 }
 output "workloads_sap_three_tier_virtual_instances_name" {
-  description = "List of name values across all workloads_sap_three_tier_virtual_instances"
-  value       = [for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : v.name]
+  description = "Map of name values across all workloads_sap_three_tier_virtual_instances, keyed the same as var.workloads_sap_three_tier_virtual_instances"
+  value       = { for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : k => v.name }
 }
 output "workloads_sap_three_tier_virtual_instances_resource_group_name" {
-  description = "List of resource_group_name values across all workloads_sap_three_tier_virtual_instances"
-  value       = [for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : v.resource_group_name]
+  description = "Map of resource_group_name values across all workloads_sap_three_tier_virtual_instances, keyed the same as var.workloads_sap_three_tier_virtual_instances"
+  value       = { for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : k => v.resource_group_name }
 }
 output "workloads_sap_three_tier_virtual_instances_sap_fqdn" {
-  description = "List of sap_fqdn values across all workloads_sap_three_tier_virtual_instances"
-  value       = [for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : v.sap_fqdn]
+  description = "Map of sap_fqdn values across all workloads_sap_three_tier_virtual_instances, keyed the same as var.workloads_sap_three_tier_virtual_instances"
+  value       = { for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : k => v.sap_fqdn }
 }
 output "workloads_sap_three_tier_virtual_instances_sap_product" {
-  description = "List of sap_product values across all workloads_sap_three_tier_virtual_instances"
-  value       = [for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : v.sap_product]
+  description = "Map of sap_product values across all workloads_sap_three_tier_virtual_instances, keyed the same as var.workloads_sap_three_tier_virtual_instances"
+  value       = { for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : k => v.sap_product }
 }
 output "workloads_sap_three_tier_virtual_instances_tags" {
-  description = "List of tags values across all workloads_sap_three_tier_virtual_instances"
-  value       = [for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : v.tags]
+  description = "Map of tags values across all workloads_sap_three_tier_virtual_instances, keyed the same as var.workloads_sap_three_tier_virtual_instances"
+  value       = { for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : k => v.tags }
 }
 output "workloads_sap_three_tier_virtual_instances_three_tier_configuration" {
-  description = "List of three_tier_configuration values across all workloads_sap_three_tier_virtual_instances"
-  value       = [for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : v.three_tier_configuration]
+  description = "Map of three_tier_configuration values across all workloads_sap_three_tier_virtual_instances, keyed the same as var.workloads_sap_three_tier_virtual_instances"
+  value       = { for k, v in azurerm_workloads_sap_three_tier_virtual_instance.workloads_sap_three_tier_virtual_instances : k => v.three_tier_configuration }
   sensitive   = true
 }
 
